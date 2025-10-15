72a351591938abe6eb9b6bb45d9967fc3ea23b20
Rishi <jaiswal11.rishikesh@gmail.com>
2025-10-15 13:40:00 -0400

    Made changes to Craps game. Added some error checking. Organised the layout a little better.

--- Misc/6.47-aGameofCraps.cpp
+++ Misc/6.47-aGameofCraps.cpp
@@ -1,100 +1,102 @@
 // Modified craps game
+// Uses Mersenne Twister random number generator
+// Uses enum class for game status
 
 #include <iostream>
-#include <cstdlib> // contains prototypes for functions srand and rand
-#include <ctime> // contains prototype for function time
 #include <random>
-using namespace std;
 
-enum class Status {CONTINUE , WON , LOST }; // all caps in constants
-Status CrapsGame(); // One game of craps
-void Instructions(); // display game instructions
-unsigned int rollDice(); // rolls dice, calculates and displays sum
+enum class      Status          {CONTINUE, WON, LOST};  // all caps in constants
+Status          CrapsGame       (std::mt19937 &engine); // One game of craps
+void            Instructions    ();                     // display game instructions
+unsigned int    rollDice        (std::mt19937 &engine);        // rolls dice, calculates and displays sum
 
 
 int main() 
-{
-    // randomize random number generator using current time
-    srand(static_cast<unsigned int>(time(0)));
+{   
+    std::mt19937 engine {std::random_device{}()}; // Mersenne Twister engine seeded with random device
     
     int bankBalance {0b1111101000}; // initial bank balance in binary. Enjoy.
     int wager       {0b0};          // initial wager amount in binary. Enjoy.   
-    char playAgain  {'y'};
-
-    cout << "Welcome to the game of Craps!\n\n";
+    
+    std::cout << "Welcome to the game of Craps!\n\n";
     Instructions();
-     
+    
+    char playAgain  {'y'};
     while ('y' == playAgain || 'Y' == playAgain)
     {
-        cout << "Enter your wager amount: $ ";
-        cin >> wager;
+        std::cout<< "Enter your wager amount: $ ";
+        std::cin >> wager;
 
-
-        bool isValidWager = (wager < 0 || wager > bankBalance);
-
-        while(isValidWager)
+        while(wager <= 0 || wager > bankBalance )
         {
-            cout << "\nInvalid wager amount. Please enter a valid amount: $";
-            cin >> wager;
+            std::cout<< "\nInvalid wager amount. Please enter a valid amount: $";
+            std::cin >> wager;
         }
-        
 
-        cout << "\nYour initial bank balance is $" << bankBalance << "\nYour wager amount is: $ " << wager << '\n';
+        std::cout<< "\nYour initial bank balance is $" << bankBalance << "\nYour wager amount is: $ " << wager << '\n';
 
         if(bankBalance)
         {
             bankBalance -= wager;
-            cout << "Your new bank balance is: $ " << bankBalance << '\n';
-            if(CrapsGame() == Status::WON)
+            std::cout<< "Your new bank balance is: $ " << bankBalance << '\n';
+            if(CrapsGame(engine) == Status::WON)
             {
                 bankBalance += 2 * wager;
-                cout << "Your new bank balance is: $ " << bankBalance << '\n';
+                std::cout<< "Your new bank balance is: $ " << bankBalance << '\n';
             }
             else
             {
-                cout << "Your new bank balance is: $ " << bankBalance << '\n';
+                std::cout<< "Your new bank balance is: $ " << bankBalance << '\n';
             }
 
         }
 
         if(bankBalance > 0)
         {
-            std::cout << "Would you like to play again? (y/n): ";
-            cin >> std::ws >> playAgain; // eat up any leading whitespace
+            std::cout << "\n\nWould you like to play again? (y/n): ";
+            std::cin  >> std::ws >> playAgain; // eat up any leading whitespace
+
+            if('n' == playAgain || 'N' == playAgain)
+            {
+                std::cout<< "\nThank you for playing! You are leaving with $ " << bankBalance << '\n' << "Goodbye!\n";
+            }
+
+            if(false == std::isalpha(static_cast<unsigned char>(playAgain)))
+            {
+                std::cout<< "\nInvalid input. Exiting game. You are leaving with $ " << bankBalance << '\n' << "Goodbye!\n";
+                return 0;
+            }
 
         }
-
+        
         else
         {
             std::cout << "You are out of money! Game over!\n";
             playAgain = 'n';
+            return 0;
         }
-
     }
-    return 0;
 }
 
 // roll dice, calculate sum and display results
-unsigned int rollDice() 
+unsigned int rollDice(std::mt19937 &engine) 
 {
-    std::mt19937 engine {std::random_device{}()}; // Mersenne Twister engine seeded with rd()
-    std::uniform_int_distribution<int> distribution(1,6);
-
+    
+    std::uniform_int_distribution<int> distribution(1,6); // uniform distribution between 1 and 6
     
     int die1{distribution(engine)}; // first die roll
     int die2{distribution(engine)}; // second die roll
-    int sum{die1 + die2}; // compute sum of die values
-
+    
     // display results of this roll
-    cout << "Player rolled " << die1 << " + " << die2
-         << " = " << sum << endl;
-    return sum;
+    std::cout<< "\n\nPlayer rolled " << die1 << " + " << die2 << " = " << die1 + die2 << "\n";
+         
+    return die1 + die2; // return sum of dice
 }
 
-Status CrapsGame()
+Status CrapsGame(std::mt19937 &engine)
 {
     unsigned int myPoint{0};
-    unsigned int sumOfDice{rollDice()}; // point if no win or loss on first roll
+    unsigned int sumOfDice{rollDice(engine)}; // point if no win or loss on first roll
     Status gameStatus; // can be CONTINUE, WON or LOST
 
     // determine game status and point (if needed) based on first roll
@@ -112,7 +114,7 @@ Status CrapsGame()
         default: // did not win or lose, so remember point
             gameStatus = Status::CONTINUE; // game is not over
             myPoint = sumOfDice; // remember the point
-            cout << "Point is " << myPoint << endl;
+            std::cout<< "\n\nPoint is " << myPoint <<"\n";
             break; // optional at end of switch  
     }
 
@@ -120,13 +122,14 @@ Status CrapsGame()
     while (Status::CONTINUE == gameStatus) 
     { 
         // not WON or LOST
-        sumOfDice = rollDice(); // roll dice again
+        sumOfDice = rollDice(engine); // roll dice again
 
         // determine game status
         if (sumOfDice == myPoint) 
         {  
             // win by making point
             gameStatus = Status::WON;
+            
         }
         else 
         {
@@ -136,27 +139,18 @@ Status CrapsGame()
                 gameStatus = Status::LOST;
             }
         }
+
     }
 
-    // display won or lost message
-    if (Status::WON == gameStatus) 
-    {
-        cout << "Player wins" << endl;
-        return Status::WON;
-    }
-    else 
-    {
-        cout << "Player loses" << endl;
-        return Status::LOST;
-    }
+    return gameStatus;
 }
 
 void Instructions()
 {
-    cout << "Instructions: \n\n\n\n " << endl;
-    cout << "A player rolls two dice. On the first roll, a 7 or 11 wins, \n" 
+    std::cout<< "Instructions: \n\n ";
+    std::cout<< "A player rolls two dice. On the first roll, a 7 or 11 wins, \n" 
          << "a 2, 3 or 12 loses, and any other roll establishes a point. \n"
          << "To win, you must continue rolling the dice until you re-roll \n"
          << "your point. You lose by rolling a 7 before making your point.\n\n\n"
-         << "Good luck!\n\n\n\n";
+         << "Good luck!\n\n";
 }
